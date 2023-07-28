cdadfebe0d1722735ee67dc887dfc60922be5ed1
doggbmx <manuelorticosta@gmail.com>
2023-07-28 16:55:50 -0400

    end class 8

--- src/es6/04-rest-spread.js
+++ src/es6/04-rest-spread.js
@@ -12,3 +12,18 @@ let user = {
 
 let { username, age } = user;
 console.log(username, age);
+
+// spread operator
+let person = { name: "Manu", age: 28 };
+let country = "PY";
+let completePerson = { ...person, country };
+console.log(completePerson);
+
+// rest
+function sum(num, ...values) {
+  console.log(values);
+  console.log(num + values[0]);
+  return num + values[0];
+}
+
+sum(1, 1, 2, 3);
