32c22fbfd1e5e433593f83424f36010f9f8374e4
doggbmx <manuelorticosta@gmail.com>
2023-07-28 16:47:26 -0400

    end class 7

---
+++ src/es6/04-rest-spread.js
@@ -0,0 +1,14 @@
+// arrays destructuring
+
+let fruits = ["banana", "apple", "orange"];
+let [a, b] = fruits;
+console.log(a, b);
+
+// objects destructuring
+let user = {
+  username: "doggbmx",
+  age: 29,
+};
+
+let { username, age } = user;
+console.log(username, age);
