1eb15891513f84d39551115665c0b84462524bad
doggbmx <manuelorticosta@gmail.com>
2023-07-31 11:29:04 -0400

    end class 9

--- src/es6/04-rest-spread.js
+++ src/es6/04-rest-spread.js
@@ -27,3 +27,18 @@ function sum(num, ...values) {
 }
 
 sum(1, 1, 2, 3);
+
+// PLAYGROUND BY PLATZI
+export function solution(
+  json1 = {
+    name: "Mr. Michi",
+    food: "Pescado",
+  },
+  json2 = {
+    age: 12,
+    color: "Blanco",
+  }
+) {
+  // Tu código aquí 👈
+  return { ...json1, ...json2 };
+}
