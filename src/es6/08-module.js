f14b98f1befb3ebafd9671a3ca904c1ecfc6b807
doggbmx <manuelorticosta@gmail.com>
2023-07-31 20:57:50 -0400

    end class 14

--- src/es6/08-module.js
+++ src/es6/08-module.js
@@ -3,3 +3,24 @@
 import hello from "./module.js";
 
 hello(); // Hello!
+
+// playground de platzi
+// exerciese.js
+// import { getData } from "./api.js";
+// export function solution() {
+//   return getData().then((list) => list);
+// }
+// api.js
+// export function getData() {
+//     return new Promise((resolve) => {
+//         resolve([{
+//             movie: 'El despertar de los michis',
+//             year: 2021,
+//             protagonista: 'Mr. Michi'
+//         },{
+//             movie: 'El despertar de los michis 2',
+//             year: 2022,
+//             protagonista: 'Mr. Michi'
+//         }]);
+//     }, 1000);
+// }
