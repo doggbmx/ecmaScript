e70b022ec284f74010dfa4d858bb8ed6cb640af3
doggbmx <manuelorticosta@gmail.com>
2023-07-31 21:27:46 -0400

    end class 18

---
+++ src/es7/01-array-includes.js
@@ -0,0 +1,10 @@
+// trabajar sobre un array y saber si existe un elemento
+
+let numbers = [1, 2, 3, 7, 8];
+// devuelve un true o false
+console.log(numbers.includes(7));
+
+const names = ["manu", "david", "oscar", "ana", "ulises"];
+// no funciona si no es exactamente igual
+// tampoco con objetos
+console.log(names.includes("Manu"));
