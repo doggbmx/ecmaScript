a7a2fa83ed0634b0521633c17a3f9b5b3773c8dd
doggbmx <manuelorticosta@gmail.com>
2023-07-31 21:42:32 -0400

    end class 19

---
+++ src/es8/00-object-entries.js
@@ -0,0 +1,15 @@
+const countries = {
+  MX: "Mexico",
+  CO: "Colombia",
+  AR: "Argentina",
+  PY: "Paraguay",
+};
+// convierte cada llave y su respectivo valor en un array
+// quedaria un array de arrays
+console.log(Object.entries(countries));
+// [
+//     [ 'MX', 'Mexico' ],
+//     [ 'CO', 'Colombia' ],
+//     [ 'AR', 'Argentina' ],
+//     [ 'PY', 'Paraguay' ]
+//   ]
---
+++ src/es8/01-object-values.js
@@ -0,0 +1,8 @@
+const countries = {
+  MX: "Mexico",
+  CO: "Colombia",
+  AR: "Argentina",
+  PY: "Paraguay",
+};
+// devuelve un array con todos los valores del objeto
+console.log(Object.values(countries)); // [ 'Mexico', 'Colombia', 'Argentina', 'Paraguay' ]
