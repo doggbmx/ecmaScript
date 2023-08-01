76c6ea9c43a6895686e8afcaa897d4a30b2d36ea
doggbmx <manuelorticosta@gmail.com>
2023-07-31 22:52:00 -0400

    end class 27

---
+++ src/es11/01-bigint.js
@@ -0,0 +1,6 @@
+// con la n al final se convierte en un bigInt, y representamos de esa forma
+const aBigNumber = 123123192481204019283213n;
+const anotherBigNumber = BigInt(123123192481204019283213);
+
+console.log(aBigNumber);
+console.log(anotherBigNumber);
---
+++ src/es11/02-nullish.js
@@ -0,0 +1,5 @@
+const anotherNumber = null;
+// validacion de nullish
+const number = anotherNumber ?? 0;
+
+console.log(number);
