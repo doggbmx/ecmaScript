bb6da89c0c91edd1497c524e3b7504c9d9b9a033
doggbmx <manuelorticosta@gmail.com>
2023-07-31 21:52:25 -0400

    end class 20

---
+++ src/es8/03-string-padding.js
@@ -0,0 +1,4 @@
+const string = "hello";
+
+console.log(string.padStart(10, "_"));
+console.log(string.padEnd(10, "_"));
---
+++ src/es8/04-trailing-commas.js
@@ -0,0 +1,3 @@
+const age = [24, 25, 26, 27, , , ,];
+console.log(age); // [ 24, 25, 26, 27, <3 empty items> ]
+console.log(age.length); // 7
