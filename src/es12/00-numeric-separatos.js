ac2563c1d12ad9235e686b47718b1eae7d054ad3
doggbmx <manuelorticosta@gmail.com>
2023-08-01 23:11:54 -0400

    end class 31

---
+++ src/es12/00-numeric-separatos.js
@@ -0,0 +1,4 @@
+const value = 100_000_000_000_000;
+// numeric separatos
+console.log(value);
+// no afecta
---
+++ src/es12/01-replace-all.js
@@ -0,0 +1,6 @@
+const string = "JavaScript es un lenguaje de programacion.";
+
+const replacedString = string.replace("JavaScript", "TypeScript");
+console.log(replacedString);
+const replacedString2 = string.replace(" ", "-");
+console.log(replacedString2);
