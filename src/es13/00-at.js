de7efc9ea50552ee81a4c6ee1305a888413c6544
doggbmx <manuelorticosta@gmail.com>
2023-08-01 23:22:19 -0400

    end class 33

---
+++ src/es13/00-at.js
@@ -0,0 +1,5 @@
+const array = ["one", "two", "three", "four", "five", "six"];
+// accedemos al ultimo a la antigua
+console.log(array[array.length - 1]); // six
+// accedemos al ultimo a la moderna
+console.log(array.at(-1)); // six
